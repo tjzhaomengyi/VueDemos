f8a79ed8d42c023eb5d8320855b9df145446f85a
mikemyzhao <tjzhaomengyi@163.com>
2019-09-06 19:03:13 +0800

    【添加】VueBlog的手脚架

---
+++ VueBlog/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+  import AddBlog from './components/AddBlog'
+
+export default {
+  name: 'App',
+  components:{
+    AddBlog
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueBlog/src/components/AddBlog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="add-blog">
+    <h2>添加博客</h2>
+    <form>
+      <label>博客标题</label>
+      <input type="text" v-model="blog.title" required/>
+      <label>博客内容</label>
+      <textarea v-model="blog.content" ></textarea>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'add-blog',
+  data(){
+    return {
+      blog:{
+        title:"",
+        content:""
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
